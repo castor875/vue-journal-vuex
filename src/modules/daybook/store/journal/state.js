3129921c169d04ee3afc9df8afded86fba9b7246
mauricio.baena <mauricio.baena@colombiasoftware.net>
2021-09-12 20:50:37 -0500

    primer commit

---
+++ src/modules/daybook/store/journal/state.js
@@ -0,0 +1,23 @@
+export default () => ({
+    isLoading: true,
+    entries: [
+        {
+            id: new Date().getTime(),
+            date: new Date().toDateString(),
+            text: 'Aut mollitia consequatur voluptatem rerum qui hic impedit a nam.' ,
+            picture: null,
+        },
+        {
+            id: new Date().getTime() + 1000,
+            date: new Date().toDateString(),
+            text: 'Et fuga enim voluptatum dolores nemo asperiores quas qui autem. Debitis consequuntur nesciunt et ipsam ea libero distinctio sit. Nihil harum vero maxime eveniet similique alias et. Voluptates qui quis enim dolorum sit ratione et. Quia incidunt saepe molestiae qui quis voluptas iure. Odit provident quia sit pariatur impedit suscipit cum.' ,
+            picture: null,
+        },
+        {
+            id: new Date().getTime() + 2000,
+            date: new Date().toDateString(),
+            text: 'Et cum dolores. Molestiae quis necessitatibus. Minima minima atque.' ,
+            picture: null,
+        },
+    ]
+})  
---
+++ src/modules/daybook/views/EntryView.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="entry-title d-flex justify-content-between p-2">
+      <div>
+          <span class="text-sucess fs-3 fw-bold">15</span>
+          <span class="mx-1 fs-3 ">Septiembre</span>
+          <span class="mx-4 fs-4 fw-light">2021, Miercoles</span>
+      </div>
+      <div>
+          <button class="btn btn-danger mx-2">
+              Borrar
+              <i class="fa fa-trash-alt"></i>
+          </button>
+
+          <button class="btn btn-primary">
+              Subir foto
+              <i class="fa fa-upload"></i>
+          </button>
+      </div>
+  </div>
+  <hr>
+  <div class="d-flex flex-column px-3 h-75">
+      <textarea placeholder="¿Que sucedió hoy?"></textarea>
+
+  </div>
+
+  <Fab 
+    icon="fa-save"/>
+  
+  <img
+    src="http://fondopantalla.com.es/file/1148/1366x768/crop/bosque-encantado.jpg"
+    alt="entry-picture"
+    class="img-thumbnail">
+
+</template>
+
+<script>
+import { defineAsyncComponent } from '@vue/runtime-core'
+export default {
+
+    components:{
+        Fab: defineAsyncComponent( () => import ('../components/Fab.vue'))
+    }
+
+}
+</script>
+
+<style lang="scss" scoped>
+    textarea{
+        font-size: 20px;
+        border: none;
+        height: 100%;
+
+        &:focus{
+            outline: none;
+        }
+    }
+
+    img{
+        width: 200px;
+        position: fixed;
+        bottom: 150px;
+        right: 20px;
+        box-shadow: 0px 5px 10px rgba($color: #000000, $alpha: 0.2);    
+    }
+
+</style>
